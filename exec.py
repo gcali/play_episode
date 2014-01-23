c6cd3fc8319262836f419c4b223c50f5ca1b017e
gcali <gcali.git@gmail.com>
2014-01-23 13:23:14 +0100

    Improve documentation of exec.py, change tests

--- exec.py
+++ exec.py
@@ -2,8 +2,23 @@
 
 import subprocess
 
-def play_video(video_path, player="echo", *opt,
+def play_video(video_path, player="mplayer", *opt,
                subtitle = None, sub_op = "-sub"):
+  """Plays a video
+     
+  This function plays a video, with an optional subtitle,
+  with the player specified in "player".
+
+  Args:
+    video_path: A string with the path of the video to be played.
+    player: Optional. The name of the executable used to play the video.
+    *opt: Optional arguments to pass to the player, before the video.
+    subtitle: Optional. If it's set to a string, that path is specified
+              as the path of the subtitle file.
+    sub_op: Optional. Used only if "subtitle" != None, the option to be
+            passed to the player to specify the subtitle.
+  """
+
   if subtitle != None:
     sub_part = [sub_op] + [subtitle]
   else:
@@ -12,4 +27,5 @@ def play_video(video_path, player="echo", *opt,
   subprocess.call(arg)
   
 if __name__ == "__main__":
-  play_video("play_episode.data", player="echo", subtitle="test")
+  play_video("play_episode.data", player="cat")
+  play_video("it", "echo", "Does", subtitle="?", sub_op = "work")
