647730f2bb676ee12b0722d1e1533b5767936514
gcali <gcali.git@gmail.com>
2014-01-23 16:10:43 +0100

    Add an option to play_video to suppress output

--- play.py
+++ play.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 
+import os
 import subprocess
 
 def play_video(video_path, player="mplayer", *opt,
-               subtitle = None, sub_op = "-sub"):
+               subtitle = None, sub_op = "-sub",
+               suppress_output = True):
   """Plays a video
      
   This function plays a video, with an optional subtitle,
@@ -17,6 +19,8 @@ def play_video(video_path, player="mplayer", *opt,
               as the path of the subtitle file.
     sub_op: Optional. Used only if "subtitle" != None, the option to be
             passed to the player to specify the subtitle.
+    suppress_output: Optional. If True, the standard output and
+                     standard error are suppressed.
   """
 
   if subtitle != None:
@@ -24,8 +28,12 @@ def play_video(video_path, player="mplayer", *opt,
   else:
     sub_part = []
   arg = [player] + list(opt) + [video_path] + sub_part
-  subprocess.call(arg)
+  if suppress_output:
+    with open(os.devnull, "w") as FNULL:
+      subprocess.call(arg, stdout=FNULL, stderr=FNULL)
+  else:
+    subprocess.call(arg)
   
 if __name__ == "__main__":
-  play_video("play_episode.data", player="cat")
-  play_video("it", "echo", "Does", subtitle="?", sub_op = "work")
+  play_video("play_episode.data", player="cat", suppress_output=False)
+  play_video("it", "echo", "Does", subtitle="?", sub_op = "work", suppress_output=False)
